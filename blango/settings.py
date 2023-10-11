bdccbb3ab1b6dae9c7a4c5ba840be7d08b964ddc
Oleksandr Chaban <oleksandr.chaban@siemens.com>
2023-10-11 06:14:52 +0000

    Finish django Allauth

--- blango/settings.py
+++ blango/settings.py
@@ -40,12 +40,18 @@ class Dev(Configuration):
         'django.contrib.contenttypes',
         'django.contrib.sessions',
         'django.contrib.messages',
+        "django.contrib.sites",
         'django.contrib.staticfiles',
         'crispy_forms',
         'crispy_bootstrap5',
         'blango_auth',
         'blog',
-        "debug_toolbar"
+        "debug_toolbar",
+
+        "allauth",
+        "allauth.account",
+        "allauth.socialaccount",
+        "allauth.socialaccount.providers.google"
     ]
 
     MIDDLEWARE = [
@@ -184,6 +190,14 @@ class Dev(Configuration):
 
     EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
     ACCOUNT_ACTIVATION_DAYS = 7
+
+    SITE_ID = 1
+
+    #tell django allauth to use email
+    ACCOUNT_USER_MODEL_USERNAME_FIELD = None
+    ACCOUNT_EMAIL_REQUIRED = True
+    ACCOUNT_USERNAME_REQUIRED = False
+    ACCOUNT_AUTHENTICATION_METHOD = "email"
 
     ALLOWED_HOSTS = ['*']
     X_FRAME_OPTIONS = 'ALLOW-FROM ' + os.environ.get('CODIO_HOSTNAME') + '-8000.codio.io'
