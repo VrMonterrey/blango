4fc236777e7569d3404fccadfd02a3128ce1a7cb
Oleksandr Chaban <oleksandr.chaban@siemens.com>
2023-10-12 06:40:22 +0000

    Finish DRF-token authentication

--- blango/settings.py
+++ blango/settings.py
@@ -52,7 +52,9 @@ class Dev(Configuration):
         "allauth",
         "allauth.account",
         "allauth.socialaccount",
-        "allauth.socialaccount.providers.google"
+        "allauth.socialaccount.providers.google",
+
+        "rest_framework.authtoken"
     ]
 
     MIDDLEWARE = [
@@ -199,6 +201,15 @@ class Dev(Configuration):
     ACCOUNT_EMAIL_REQUIRED = True
     ACCOUNT_USERNAME_REQUIRED = False
     ACCOUNT_AUTHENTICATION_METHOD = "email"
+
+
+    REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.BasicAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
+    ]
+}
 
     ALLOWED_HOSTS = ['*']
     X_FRAME_OPTIONS = 'ALLOW-FROM ' + os.environ.get('CODIO_HOSTNAME') + '-8000.codio.io'
