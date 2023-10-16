f4719a5355de754b4065df012199dbfae2793379
Oleksandr Chaban <oleksandr.chaban@siemens.com>
2023-10-16 14:33:47 +0000

    Finish browsable API

--- blango/settings.py
+++ blango/settings.py
@@ -54,7 +54,9 @@ class Dev(Configuration):
         "allauth.socialaccount",
         "allauth.socialaccount.providers.google",
 
-        "rest_framework.authtoken"
+        "rest_framework.authtoken",
+
+        'drf_yasg'
     ]
 
     MIDDLEWARE = [
@@ -212,6 +214,13 @@ class Dev(Configuration):
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticatedOrReadOnly"
     ],
+}
+
+    SWAGGER_SETTINGS = {
+    "SECURITY_DEFINITIONS": {
+        "Token": {"type": "apiKey", "name": "Authorization", "in": "header"},
+        "Basic": {"type": "basic"},
+    }
 }
 
     ALLOWED_HOSTS = ['*']
